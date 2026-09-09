b036a7172a6749d0d9de5e7364a098a981a99e55
fatemeh <fattemehnazariii@gmail.com>
2026-09-09 16:29:53 +0330

    fix password bug

--- script.js
+++ script.js
@@ -21,12 +21,40 @@ function updateProgress(step) {
     }
   });
 }
+function validatePassword(password) {
+  const minLength = 8;
+  const hasUpperCase = /[A-Z]/.test(password);
+  const hasLowerCase = /[a-z]/.test(password);
+  const hasNumber = /[0-9]/.test(password);
+
+  if (password.length < minLength) {
+    return "رمز عبور باید حداقل ۸ کاراکتر باشد";
+  }
+  if (!hasUpperCase) {
+    return "رمز عبور باید حداقل یک حرف بزرگ داشته باشد";
+  }
+  if (!hasLowerCase) {
+    return "رمز عبور باید حداقل یک حرف کوچک داشته باشد";
+  }
+  if (!hasNumber) {
+    return "رمز عبور باید حداقل یک عدد داشته باشد";
+  }
+  return null; // یعنی معتبره
+}
+
 function validateFields(step) {
   const fields = pages[step].querySelectorAll("input,select");
   for (let field of fields) {
     if (!field.value.trim()) {
       alert("please fill in all fields");
       return false;
+    }
+    if (field.type === "password") {
+      const errorMsg = validatePassword(field.value);
+      if (errorMsg) {
+        alert(errorMsg);
+        return false;
+      }
     }
   }
   return true;
@@ -41,7 +69,7 @@ nextBtns.forEach((btn, index) => {
   });
 });
 prevBtns.forEach((btn, index) => {
-  btn.addEventListener("clicl", (e) => {
+  btn.addEventListener("click", (e) => {
     e.preventDefault();
     currentstep--;
     showPage(currentstep);
